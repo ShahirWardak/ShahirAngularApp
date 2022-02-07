7d05b8378340aa5326c982b722cc3756f141df94
ShahirWardak <shahir.wardak@rspb.org.uk>
2022-02-07 13:32:51 +0000

    Added temp styles & removed zip artifact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadingBannerComponent } from './heading-banner/heading-banner.component';
+import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadingBannerComponent,
+    ImageCarouselComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/heading-banner/heading-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heading-banner',
+  templateUrl: './heading-banner.component.html',
+  styleUrls: ['./heading-banner.component.scss']
+})
+export class HeadingBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
